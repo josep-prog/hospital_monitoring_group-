353a10e0d5ac46d9ee33f28297a1d77fd9b17baa
Joseph Nishimwe <j.nishimwe@alustudent.com>
2024-10-10 01:58:46 -0700

    task3

---
+++ backup_archives.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Directory for archived logs
+group_number=1  # Replace with your group number
+archive_dir="archived_logs_group${group_number}"
+
+# Create the archive directory if it doesn't exist
+mkdir -p "$archive_dir"
+
+# Move archived log files
+mv heart_rate_log.txt_* "$archive_dir" 2>/dev/null
+
+# Remote server details
+remote_user="Joseph Nishimwe"  # Replace with your sandbox username
+remote_host="https://a8dbe5e653dc.601a9382.alu-cod.online/"  # write your sandbox host
+remote_dir="/home/"
+
+# Backup to remote server
+scp "$archive_dir/*" "$remote_user@$remote_host:$remote_dir"
+
+echo "Backup completed."
