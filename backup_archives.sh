8012ab3b229b63a37197310086742b839c5549a2
Joseph Nishimwe <j.nishimwe@alustudent.com>
2024-10-10 02:16:45 -0700

    task 3

--- backup_archives.sh
+++ backup_archives.sh
@@ -12,7 +12,7 @@ mv heart_rate_log.txt_* "$archive_dir" 2>/dev/null
 
 # Remote server details
 remote_user="a8dbe5e653dc"  # Replace with your sandbox username
-remote_host="a8dbe5e653dc.601a9382.alu-cod.online"  # write your sandbox host
+remote_host="https://a8dbe5e653dc.601a9382.alu-cod.online/"  # write your sandbox host
 remote_dir="/home/"
 
 # Backup to remote server
