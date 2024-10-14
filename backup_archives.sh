b7425dfec471ba155d4410ff9b966964f0029515
Joseph Nishimwe <j.nishimwe@alustudent>
2024-10-14 01:36:10 -0700

    summative work

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Define variables
-GROUP_NUMBER="YOUR_GROUP_NUMBER"  # Replace with your actual group number
-ARCHIVE_DIR="archived_logs_group$GROUP_NUMBER"   
-REMOTE_USER="ba704bbbf90a"        # my username from sandbox
-REMOTE_HOST="ba704bbbf90a.c0bb8d29.alu-cod.online"            # myhostname from sandbox password 
+GROUP_NUMBER="9"  # Your group number
+ARCHIVE_DIR="archived_logs_group9”
+REMOTE_USER="ba704bbbf90a"  # my  username
+REMOTE_HOST="ba704bbbf90a.c0bb8d29.alu-cod.online"  # my host
 REMOTE_DIR="/home/"
 
 # Create the archived logs directory if it doesn't exist
-mkdir -p "$ARCHIVE_DIR"
+mkdir -p archived_logs_group9
 
 # Move all archived log files to the designated directory
-# Assuming archived log files have a specific timestamped pattern, e.g., *.log.tar.gz
-mv *archive*.log.tar.gz "$ARCHIVE_DIR"
+# Assuming archived log files have the format heart_rate_log.txt_YYYYMMDD_HHMMSS
+mv heart_rate_log.txt_*  archived_logs_group9
 
 # Check if the move was successful
 if [ $? -ne 0 ]; then
@@ -21,7 +21,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Backup the archived logs to the remote server
-scp -r "$ARCHIVE_DIR/" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
+scp -r archived_logs_group9/   ba704bbbf90a@ba704bbbf90a.c0bb8d29.alu-cod.online:/home/
 
 # Check if the SCP command was successful
 if [ $? -eq 0 ]; then
